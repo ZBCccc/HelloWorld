05051d2e6ec75c198afffc22e3c7e6d831da5141
ZBCccc <1641671577@qq.com>
2025-02-08 19:39:50 +0800

    Initial commit

---
+++ Code1_1/src/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main() {
+    printf("Hello, World!\n");
+    printf("Hello, C++!\n");
+    printf("Hello, Cyan!\n");
+    cout << "重新学习C++第一天！" << endl;
+    cout << "return 0 可以忽略不写" << endl;
+}
